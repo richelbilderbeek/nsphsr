255f60b40e51e72bc66147bdf2e9dcc4db6e4dee
richelbilderbeek <richel@richelbilderbeek.nl>
2021-10-25 16:43:49 +0200

    Builds locally

---
+++ R/check_data.R
@@ -0,0 +1,7 @@
+#' Function that checks that the data is fine
+check_data <- function() {
+  # TODO:
+  # pea_1
+  # pea_2
+  # pea_3
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(nsphsr)
+
+test_check("nsphsr")
